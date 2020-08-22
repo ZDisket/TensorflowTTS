76ec8b8c2a74c2a72736606a5c5ad5e43d24a371
ZDisket <xml506ok@gmail.com>
2020-08-22 16:48:56 -0300

    Fix LJSpeechProcessor

--- tensorflow_tts/processor/ljspeech.py
+++ tensorflow_tts/processor/ljspeech.py
@@ -1,15 +1,28 @@
 # -*- coding: utf-8 -*-
-# This code is copy and modify from https://github.com/keithito/tacotron.
-"""Perform preprocessing and raw feature extraction."""
+# Copyright 2020 TensorFlowTTS Team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Perform preprocessing and raw feature extraction for LJSpeech dataset."""
 
 import os
 import re
 
 import numpy as np
 import soundfile as sf
-from g2p_en import G2p
-
+from dataclasses import dataclass
+from tensorflow_tts.processor import BaseProcessor
 from tensorflow_tts.utils import cleaners
+from g2p_en import G2p
 
 valid_symbols = [
     "AA",
@@ -100,53 +113,56 @@ valid_symbols = [
     "END", #padding token
 ]
 
-_pad = "_"
-_eos = "~"
+_pad = "pad"
+_eos = "eos"
 _punctuation = "!'(),.:;? "
 _special = "-"
 _letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-_singlesil = [",",";","?",".","..."]
+
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 _arpabet = ["@" + s for s in valid_symbols]
 
 # Export all symbols:
-symbols = (
+LJSPEECH_SYMBOLS = (
     [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet + [_eos]
 )
-
-# Mappings from symbol to numeric ID and vice versa:
-_symbol_to_id = {s: i for i, s in enumerate(symbols)}
-_id_to_symbol = {i: s for i, s in enumerate(symbols)}
 
 # Regular expression matching text enclosed in curly braces:
 _curly_re = re.compile(r"(.*?)\{(.+?)\}(.*)")
 
 
-class LJSpeechProcessor(object):
+@dataclass
+class LJSpeechProcessor(BaseProcessor):
     """LJSpeech processor."""
 
-    def __init__(self, data_dir, cleaner_names, metadata_filename="metadata.csv"):
-        self.data_dir = data_dir
-        self.cleaner_names = cleaner_names
+    cleaner_names: str = "english_cleaners"
+    positions = {
+        "wave_file": 0,
+        "text": 1,
+        "text_norm": 2,
+    }
+    train_f_name: str = "metadata.csv"
 
-        self.speaker_name = "ljspeech"
-        if data_dir:
-            with open(os.path.join(data_dir, metadata_filename), encoding="utf-8") as f:
-                self.items = [self.split_line(data_dir, line, "|") for line in f]
+    def create_items(self):
+        if self.data_dir:
+            with open(
+                os.path.join(self.data_dir, self.train_f_name), encoding="utf-8"
+            ) as f:
+                self.items = [self.split_line(self.data_dir, line, "|") for line in f]
 
     def split_line(self, data_dir, line, split):
-        wav_file, _, text_norm = line.strip().split(split)
-        wav_path = os.path.join(data_dir, "wavs", f"{wav_file}.wav")
-        return text_norm, wav_path, self.speaker_name
-
-    def setup_eos_token(self):
-        return _eos
+        parts = line.strip().split(split)
+        wave_file = parts[self.positions["wave_file"]]
+        text_norm = parts[self.positions["text_norm"]]
+        wav_path = os.path.join(data_dir, "wavs", f"{wave_file}.wav")
+        speaker_name = "ljspeech"
+        return text_norm, wav_path, speaker_name
 
     def get_one_sample(self, item):
-        text, wav_file, speaker_name = item
+        text, wav_path, speaker_name = item
 
         # normalize audio signal to be [-1, 1], soundfile already norm.
-        audio, rate = sf.read(wav_file)
+        audio, rate = sf.read(wav_path)
         audio = audio.astype(np.float32)
 
         # convert text to ids
@@ -156,7 +172,7 @@ class LJSpeechProcessor(object):
             "raw_text": text,
             "text_ids": text_ids,
             "audio": audio,
-            "utt_id": os.path.split(wav_file)[-1].split(".")[0],
+            "utt_id": os.path.split(wav_path)[-1].split(".")[0],
             "speaker_name": speaker_name,
             "rate": rate,
         }
@@ -164,24 +180,33 @@ class LJSpeechProcessor(object):
         return sample
 
     def text_to_sequence(self, text):
-        global _symbol_to_id
-
         sequence = []
         # Check for curly braces and treat their contents as ARPAbet:
         while len(text):
             m = _curly_re.match(text)
             if not m:
-                sequence += _symbols_to_sequence(
-                    _clean_text(text, [self.cleaner_names])
+                sequence += self._symbols_to_sequence(
+                    self._clean_text(text, [self.cleaner_names])
                 )
                 break
-            sequence += _symbols_to_sequence(
-                _clean_text(m.group(1), [self.cleaner_names])
+            sequence += self._symbols_to_sequence(
+                self._clean_text(m.group(1), [self.cleaner_names])
             )
-            sequence += _arpabet_to_sequence(m.group(2))
+            sequence += self._arpabet_to_sequence(m.group(2))
             text = m.group(3)
+
+        # add eos tokens
+        sequence += [self.eos_id]
         return sequence
-      
+
+    def _clean_text(self, text, cleaner_names):
+        for name in cleaner_names:
+            cleaner = getattr(cleaners, name)
+            if not cleaner:
+                raise Exception("Unknown cleaner: %s" % name)
+            text = cleaner(text)
+        return text
+
     def processtxtph(self,intxt):
       g2p = G2p()
       ptext =  _clean_text(intxt,[self.cleaner_names])
@@ -191,18 +216,18 @@ class LJSpeechProcessor(object):
       ids = _arpabet_to_sequence(arpatxt)
 
       return ids, arpatxt
-  
-  
-  
 
+    def setup_eos_token(self):
+        return _eos # because we do use this 
 
-def _clean_text(text, cleaner_names):
-    for name in cleaner_names:
-        cleaner = getattr(cleaners, name)
-        if not cleaner:
-            raise Exception("Unknown cleaner: %s" % name)
-        text = cleaner(text)
-    return text
+    def _symbols_to_sequence(self, symbols):
+        return [self.symbol_to_id[s] for s in symbols if self._should_keep_symbol(s)]
+
+    def _arpabet_to_sequence(self, text):
+        return self._symbols_to_sequence(["@" + s for s in text.split()])
+
+    def _should_keep_symbol(self, s):
+        return s in self.symbol_to_id and s != "_" and s != "~"
 
 def _g2p2synth(inseq):
   phseq = list()
@@ -222,17 +247,3 @@ def _g2p2synth(inseq):
     
   phseq.append("SIL")
   return phseq
-
-
-
-
-def _symbols_to_sequence(symbols):
-    return [_symbol_to_id[s] for s in symbols if _should_keep_symbol(s)]
-
-
-def _arpabet_to_sequence(text):
-    return _symbols_to_sequence(["@" + s for s in text.split()])
-
-
-def _should_keep_symbol(s):
-    return s in _symbol_to_id and s != "_" and s != "~"
