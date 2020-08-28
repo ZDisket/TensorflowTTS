3471204ceca2ca823f319e50b4dea696ec381d31
ZDisket <xml506ok@gmail.com>
2020-08-28 20:37:45 -0300

    PreMFA export word list

--- examples/fastspeech2/mfa/premfa.py
+++ examples/fastspeech2/mfa/premfa.py
@@ -19,9 +19,17 @@ def main():
         type=str,
         help="Directory to create for TextGrid output",
     )
+    parser.add_argument(
+        "--export-wordlist",
+        default="",
+        type=str,
+        help="If not empty, will export a word list to the file",
+    )
     args = parser.parse_args()
     metadpath = args.dataset_path + "/metadata.csv"
     datapath = args.dataset_path + "/wavs"
+    dowordlist = len(args.export_wordlist) > 2
+    words = list()
 
     safemkdir(args.out_path)
     print("Preparing dataset for MFA...")
@@ -37,6 +45,20 @@ def main():
         fout = open(pfileout,"w")
         fout.write(transcr + "\n")
         fout.close()
+        if dowordlist:
+          for w in transcr.split(" "):
+            if w not in words:
+              words.append(w)
+    
+    if len(words) > 1:
+      print("Exporting words...")
+      wordsout = open(args.export_wordlist,"w")
+      for w in tqdm(words):
+        wordsout.write(w + "\n")
+      print("Done!")
+      wordsout.close()
+
+        
 
 if __name__ == "__main__":
     main()
